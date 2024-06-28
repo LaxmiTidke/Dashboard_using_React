9c73b90f1d7ae84430c536d882ceb0f670943e99
LaxmiTidke <123560554+LaxmiTidke@users.noreply.github.com>
2024-06-28 09:15:38 +0530

    first commit

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line react-refresh/only-export-components
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+const App = () => {
+    return (
+        <>
+          <div>
+               <Navigation/>
+               <Hero/>
+          </div>
+       </>
+    ) 
+};
+export default App;
---
+++ dashboard/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+const Navigation = () =>{
+
+    return (
+      <>
+        <nav className="container">
+                <div className="logo">
+                    <img src="/images/Logoolaxmi.png" alt= "logo"/>
+                </div>
+
+                <ul>
+                   <li href="#">NEW ARRIVALS</li> 
+                   <li href="#">BRANDS</li> 
+                   <li href="#">MENS</li> 
+                   <li href="#">WOMEN</li> 
+                </ul>
+                <button>LOGIN</button>
+               </nav>
+     </>
+    );
+
+    
+};
+export default Navigation;
